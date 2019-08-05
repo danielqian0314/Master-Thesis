72873f63067b5c7eee49b7d86d029e58276f3875
danielqian0314 <39278120+danielqian0314@users.noreply.github.com>
2019-08-05 14:10:43 +0200

    Update training_prepare.py

--- treatmentresponse/training_prepare.py
+++ treatmentresponse/training_prepare.py
@@ -37,28 +37,28 @@ def get_data_MR_2D(patientIDs):
 
     return input,output
 
-def get_data_central_MR_2D(patientIDs):
-    patientsMR=np.load(data_path+'patientsMR.npy').astype(int)
-    MR2d=np.load(data_path+'MR2d.npy')
-    PETMR2d=np.load(data_path+'PETMR2d.npy')
-    input_MR2d=[]
-    input_PETMR2d=[]
+def get_data_central_CT_2D(patientIDs):
+    patientsCT=np.load(data_path+'patientsCT.npy').astype(int)
+    CT2d=np.load(data_path+'CT2d.npy')
+    PETCT2d=np.load(data_path+'PETCT2d.npy')
+    input_CT2d=[]
+    input_PETCT2d=[]
     for k in range(len(patientIDs)):        
-        for i in range(len(patientsMR)):            
-            if patientsMR[i]==patientIDs[k]:
-                 where = np.array(np.where(MR2d[i*64:(i+1)*64]))
+        for i in range(len(patientsCT)):            
+            if patientsCT[i]==patientIDs[k]:
+                 where = np.array(np.where(CT2d[i*64:(i+1)*64]))
                  x2, y2, z2 = np.amax(where, axis=1)
                 central_num=int(x2//2)
-                input_MR2d.extend(MR2d[i*64+central_num-1:i*64+central_num+1])
-                input_PETMR2d.extend(PETMR2d[i*64+central_num-1:i*64+central_num+1])
+                input_CT2d.extend(MR2d[i*64+central_num-1:i*64+central_num+1])
+                input_PETCT2d.extend(PETCT2d[i*64+central_num-1:i*64+central_num+1])
                 break
     
-    input_MR2d=np.asarray(input_MR2d)
-    input_PETMR2d=np.asarray(input_PETMR2d)
+    input_CT2d=np.asarray(input_CT2d)
+    input_PETCT2d=np.asarray(input_PETCT2d)
 
-    input_image=np.zeros((input_MR2d.shape[0],input_MR2d.shape[1],input_MR2d.shape[2],2))
-    input_image[:,:,:,0]=input_MR2d
-    input_image[:,:,:,1]=input_PETMR2d
+    input_image=np.zeros((input_CT2d.shape[0],input_CT2d.shape[1],input_CT2d.shape[2],2))
+    input_image[:,:,:,0]=input_CT2d
+    input_image[:,:,:,1]=input_PETCT2d
 
     input=[input_image]    
     for meta in label.getMetadata(patientIDs):
